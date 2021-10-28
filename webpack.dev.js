34e58748086fe3966dd1c31dbd903d6a44a873f3
Donna Lorenzo <dlorenzo24@hotmail.com>
2021-10-28 09:29:24 -0500

    configured webpack files and download all dependencies

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+
+module.exports = merge(common, {
+    mode: 'development',
+    output:{
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'node-sass', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
+})
